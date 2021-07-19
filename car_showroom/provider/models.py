3d6c722a50bd22c5f83a40e4874d266109c94bd5
KarimKkhalifa <kkhalifakarim@gmail.com>
2021-07-19 16:04:09 +0300

    first version of models

---
+++ car_showroom/provider/models.py
@@ -0,0 +1,61 @@
+from django.core.validators import MinValueValidator, MaxValueValidator
+from django.db import models
+
+
+class Manufacturer(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    is_active = models.BooleanField(default=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    modification_time = models.DateTimeField(auto_now=True)
+    description = models.TextField(blank=True)
+    location = models.ForeignKey(
+        "showroom.Location",
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="+",
+    )
+    foundation_time = models.DateTimeField()
+
+    def __str__(self):
+        return self.name
+
+
+class Car(models.Model):
+    manufacturer = models.ForeignKey(
+        Manufacturer,
+        on_delete=models.PROTECT,
+        related_name="cars",
+    )
+    model = models.CharField(max_length=50)
+    carcase = models.CharField(max_length=25)
+    state = models.CharField(max_length=25)
+    price = models.DecimalField(max_digits=10, decimal_places=2)
+    is_active = models.BooleanField(default=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    modification_time = models.DateTimeField(auto_now=True)
+    production_date = models.DateTimeField(
+        validators=(MinValueValidator(1950), MaxValueValidator(2021))
+    )
+
+    def __str__(self):
+        return self.model
+
+
+class Provider(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    is_active = models.BooleanField(default=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    modification_time = models.DateTimeField(auto_now=True)
+    description = models.TextField(blank=True)
+    foundation_time = models.DateTimeField()
+    cars = models.ManyToManyField(
+        Car,
+        related_name="providers",
+    )
+    showrooms = models.ManyToManyField(
+        "showroom.Showroom",
+        related_name="providers",
+    )
+
+    def __str__(self):
+        return self.name
---
+++ car_showroom/showroom/models.py
@@ -0,0 +1,85 @@
+from django_countries.fields import CountryField
+from django.db import models
+
+from provider.models import Car
+
+
+class Location(models.Model):
+    country = CountryField()
+    city = models.CharField(max_length=150)
+    street = models.CharField(max_length=150)
+    building_number = models.PositiveSmallIntegerField()
+
+    def __str__(self):
+        return f"{self.country} {self.city} {self.street} {self.building_number}"
+
+
+class Showroom(models.Model):
+    name = models.CharField(max_length=150, unique=True)
+    is_active = models.BooleanField(default=True)
+    creation_time = models.DateTimeField(auto_now_add=True)
+    modification_time = models.DateTimeField(auto_now=True)
+    balance = models.DecimalField(max_digits=10, decimal_places=2)
+    preferred_model = models.CharField(max_length=50)
+    preferred_carcase = models.CharField(max_length=50)
+    preferred_state = models.CharField(max_length=50)
+    location = models.ForeignKey(
+        Location, on_delete=models.SET_NULL, related_name="+", null=True
+    )
+    available_cars = models.ManyToManyField(
+        Car,
+        through="ShowroomSaleHistory",
+        through_fields=("showroom", "sold_car"),
+        related_name="car_showrooms"
+    )
+    customers = models.ManyToManyField(
+        "user.User",
+        related_name="car_showrooms",
+    )
+
+    providers = models.ManyToManyField(
+        "provider.Provider", null=True, related_name="car_showrooms"
+    )
+
+    def __str__(self):
+        return self.name
+
+
+class ShowroomSaleHistory(models.Model):
+    cars_quantity = models.PositiveSmallIntegerField()
+    sold_car = models.ForeignKey(
+        Car,
+        on_delete=models.PROTECT,
+        related_name="+",
+    )
+    customer = models.ForeignKey(
+        "user.User",
+        on_delete=models.SET_NULL,
+        related_name="purchases",
+        null=True,
+    )
+    showroom = models.ForeignKey(
+        Showroom, on_delete=models.CASCADE, related_name="sales"
+    )
+
+    def __str__(self):
+        return self.sold_car
+
+
+class ShowroomPurchaseHistory(models.Model):
+    cars_quantity = models.PositiveSmallIntegerField()
+    showroom = models.ForeignKey(
+        Showroom, on_delete=models.CASCADE, related_name="purchases"
+    )
+    bought_car = models.ForeignKey(
+        Car, on_delete=models.SET_NULL, related_name="+", null=True
+    )
+    provider = models.ForeignKey(
+        "provider.Provider",
+        on_delete=models.SET_NULL,
+        related_name="sales",
+        null=True,
+    )
+
+    def __str__(self):
+        return self.bought_car
